9f6a2bfe7f84a8754a1c1fcaa3f81ce3548c017d
hitchhooker <tommi@niemi.lol>
2024-12-21 10:48:07 +0700

    force commit 1734752887

--- aliases.nix
+++ aliases.nix
@@ -5,15 +5,13 @@
     vi = "nvim";
     rbt = "sudo nixos-rebuild dry-build";
     rbs = ''
-      bash -c '
-      cd /etc/nixos
-      git add -A
-      nixos_version=$(nixos-version)
-      build_output=$(sudo nixos-rebuild switch 2>&1)
-      build_path=$(echo "$build_output" | grep -oE "/nix/store/[a-z0-9]{32}-nixos-system-[^ ]+")
-      git commit -m "nixos ${nixos_version}, build ${build_path}"
-      '
-      '';
+      cd /etc/nixos && \
+      git add -A && \
+      nixos_version=$(nixos-version) && \
+      build_output=$(sudo nixos-rebuild switch 2>&1) && \
+      build_path=$(echo "$build_output" | grep -oE "/nix/store/[a-z0-9]{32}-nixos-system-[^ ]+") && \
+      git commit -m "nixos $nixos_version, build $build_path"
+    '';
     nx = ''cd /etc/nixos/ && ls'';
   };
 }
