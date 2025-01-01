eafc9923ade3fc96c6d991bb02310ac1025dd0df
hitchhooker <tommi@niemi.lol>
2025-01-02 05:15:31 +0700

    nixos 24.05.7139.bcba2fbf6963 (Uakari), build /nix/store/4cz69skxamdgi0i6snkcjmq1y90pa62a-nixos-system-lenovo-24.05.7139.bcba2fbf6963.drv /nix/store/4cz69skxamdgi0i6snkcjmq1y90pa62a-nixos-system-lenovo-24.05.7139.bcba2fbf6963.drv'...

--- aliases.nix
+++ aliases.nix
@@ -6,11 +6,11 @@
     rbt = "sudo nixos-rebuild dry-build";
     rbs = ''
       cd /etc/nixos && \
-      nixos_version=$(nixos-version) && \
+      echo 'rbs...' && \
       build_output=$(sudo nixos-rebuild switch 2>&1) && \
       build_path=$(echo "$build_output" | grep -oE "/nix/store/[a-z0-9]{32}-nixos-system-[^ ]+") && \
       git add -A && \
-      git commit -m "nixos $nixos_version, build $build_path"
+      git commit -m "$build_path"
     '';
     nx = ''cd /etc/nixos/ && ls'';
   };
