47f10f82a6bfa06150c6cd123417d7040928ae9f
hitchhooker <tommi@niemi.lol>
2025-03-10 00:36:40 +0700

    update:

--- configuration.nix
+++ configuration.nix
@@ -7,11 +7,12 @@ let
                   else ./hardware/default.nix;
   stable = import <nixos> { config = config.nixpkgs.config; };
   unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
+  agenix-src = builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz";
+  agenix = import agenix-src {};
 in
 {
   # imports
-  imports = [ ./shell.nix <home-manager/nixos> useHostConfig
-  (builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz")
+  imports = [ ./shell.nix <home-manager/nixos> useHostConfig agenix
   ];
 
   age.secrets.alice-hash = {
