6b0fa085bf653bfdc507a8f0b079c6cb2ecb3344
hitchhooker <tommi@niemi.lol>
2025-03-19 07:18:59 +0700

    update: /nix/store/x5hhxrgrzvlivh0n0pb8nj5hx23dw9r9-nixos-system-lenovo-25.05beta767267.6607cf789e54

--- dotfiles/zsh/.zshrc
+++ dotfiles/zsh/.zshrc
@@ -10,27 +10,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="chaos"
 
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
-# Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
-# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
-
-# Uncomment the following line to change how often to auto-update (in days).
-# zstyle ':omz:update' frequency 13
-
+zstyle ':omz:update' mode disabled  # disable automatic updates
 # Uncomment the following line if pasting URLs and other text is messed up.
 # DISABLE_MAGIC_FUNCTIONS="true"
 
@@ -53,17 +33,6 @@ ZSH_THEME="chaos"
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
 # DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
 
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
--- hardware/lenovo.nix
+++ hardware/lenovo.nix
@@ -1,73 +1,65 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-
 {
   imports = [
     "${modulesPath}/installer/scan/not-detected.nix"
   ];
 
-# Networking settings
-  networking = {
-    hostName = "lenovo";
-    useDHCP = lib.mkDefault true;
-    wireless = {
-      enable = true;
+  # HARDWARE CONFIGURATION
+  # ----------------------
+
+  # Boot and Kernel
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
     };
-  };
-
-  security.sudo = {
-    enable = true;
-    extraConfig = ''
-      alice ALL=(ALL) NOPASSWD: ALL
-      user ALL=(ALL) NOPASSWD: ALL
-      '';
-  };
-
-# Bootloader and kernel settings
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
-    };
-  };
-
-  boot.initrd = {
-    availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-    luks.devices.encrypted_partition.device = "/dev/disk/by-uuid/da1a7fdf-43a5-4f84-ac6f-7447243a2c31";
-  };
-
-  #boot.kernelPackages = pkgs.linuxPackages_6_13;
-  boot.kernelModules = [ "amd_pstate" "amdgpu" "kvm-amd" "acpi_cpufreq" "i2c_hid" "k10temp" "thinkpad_acpi" "pmouse" ];
-  boot.extraModulePackages = [ pkgs.linuxPackages.acpi_call ];
-
-# tlp for power management (lenovo laptops)
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      RESTORE_DEVICE_STATE_ON_STARTUP = true;
-    };
-  };
-
-# fancontrol
-  services.thinkfan = {
-    enable = true;
-    settings = {
-      hwmon = [
-      { device = "/sys/class/hwmon/hwmon9/pwm1"; }
+    
+    initrd = {
+      availableKernelModules = [ 
+        "nvme" 
+        "xhci_pci" 
+        "usb_storage" 
+        "sd_mod" 
+        "rtsx_pci_sdmmc" 
       ];
-      levels = [
-      { temperature = 55; level = 0; }
-      { temperature = 60; level = 1; }
-      { temperature = 70; level = 2; }
-      { temperature = 80; level = 7; }
-      ];
+      # Disk encryption
+      luks.devices.encrypted_partition.device = "/dev/disk/by-uuid/da1a7fdf-43a5-4f84-ac6f-7447243a2c31";
     };
+    
+    # Kernel configuration
+    # kernelPackages = pkgs.linuxPackages_6_13; # Uncomment to enable newer kernel
+    kernelModules = [ 
+      "kvm-amd"      # Virtualization
+      "amd_pstate"   # Power management
+      "acpi_cpufreq" 
+      "amdgpu"       # GPU support
+      "k10temp"      # Temperature monitoring
+      "thinkpad_acpi"
+      "i2c_hid"      # Input devices
+      "pmouse"
+    ];
+    extraModulePackages = [ pkgs.linuxPackages.acpi_call ];
+    
+    # AMD-specific kernel parameters
+    kernelParams = [
+      # GPU configuration
+      "radeon.si_support=0"
+      "amdgpu.si_support=1"
+      "radeon.cik_support=0"
+      "amdgpu.cik_support=1"
+      "amdgpu.dc=1"
+      
+      # CPU configuration
+      "nosmt=force"        # Disable simultaneous multithreading for security
+      "amd_pstate=passive" # Power management
+    ];
   };
 
-# Filesystem configurations
+  # File systems
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-uuid/4c77ddb8-967c-496c-a7b3-566fcbd55255";
@@ -84,51 +76,99 @@
     };
   };
 
-# Swap configuration
+  # Swap
   swapDevices = [
-  { device = "/dev/disk/by-uuid/6f3e1022-45d0-4378-bcf5-26e4bd42bfd4"; }
+    { device = "/dev/disk/by-uuid/6f3e1022-45d0-4378-bcf5-26e4bd42bfd4"; }
   ];
 
+  # GRAPHICS AND DISPLAY
+  # --------------------
+  
+  # AMD GPU configuration
   services.xserver.videoDrivers = [ "amdgpu" ];
-  boot.kernelParams = [
-    "radeon.si_support=0"
-      "amdgpu.si_support=1"
-      "radeon.cik_support=0"
-      "amdgpu.cik_support=1"
-      "amdgpu.dc=1"  # Enable display core support
-      "nosmt=force"  # Disable SMT if needed
-      "amd_pstate=passive"  # Power management for AMD
-  ];
-
-
-# Enable Bluetooth and Audio
-  hardware.bluetooth.enable = true;
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-        amdvlk
-        vaapiVdpau
-        libvdpau-va-gl
+      amdvlk
+      vaapiVdpau
+      libvdpau-va-gl
     ];
     extraPackages32 = with pkgs; [
       driversi686Linux.amdvlk
     ];
   };
+  
+  # Input devices
+  services.libinput = {
+    enable = true;
+    touchpad = {
+      naturalScrolling = false;
+      tapping = true;
+      disableWhileTyping = true;
+      scrollMethod = "twofinger";
+    };
+  };
 
-# Sound (PipeWire)
+  # SYSTEM SERVICES
+  # ---------------
+  
+  # Power management (ThinkPad-specific)
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      RESTORE_DEVICE_STATE_ON_STARTUP = true;
+    };
+  };
+
+  # Fan control (ThinkPad-specific)
+  services.thinkfan = {
+    enable = true;
+    settings = {
+      hwmon = [
+        { device = "/sys/class/hwmon/hwmon9/pwm1"; }
+      ];
+      levels = [
+        { temperature = 55; level = 0; }
+        { temperature = 60; level = 1; }
+        { temperature = 70; level = 2; }
+        { temperature = 80; level = 7; }
+      ];
+    };
+  };
+
+  # Audio with PipeWire
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
-# Touchpad-specific settings
-  services.libinput = {
+  # Peripheral device support
+  hardware.bluetooth.enable = true;
+  services.udev.packages = with pkgs; [
+    headsetcontrol
+  ];
+
+  # NETWORKING
+  # ----------
+  
+  networking = {
+    hostName = "lenovo";
+    useDHCP = lib.mkDefault true;
+    wireless.enable = true;
+  };
+
+  # SECURITY
+  # --------
+  
+  # sudo configuration - SECURITY ISSUE: passwordless sudo is a security risk
+  # Recommendation: Replace with more restrictive rules or proper authentication
+  security.sudo = {
     enable = true;
-    touchpad = {
-      naturalScrolling = false;
-      tapping = true;                # Enable tap-to-click
-        disableWhileTyping = true;   # Disable touchpad while typing
-        scrollMethod = "twofinger";  # Use two-finger scrolling
-    };
+    extraConfig = ''
+      alice ALL=(ALL) NOPASSWD: ALL
+      user ALL=(ALL) NOPASSWD: ALL
+    '';
   };
 }
