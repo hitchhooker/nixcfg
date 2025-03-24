cc8dffcbe9eab0d38fe0addb14a52d2ca0738770
hitchhooker <tommi@niemi.lol>
2025-03-24 09:35:39 +0700

    update:

--- configuration.nix
+++ configuration.nix
@@ -158,7 +158,7 @@ in
     unstable.rustup unstable.wget unstable.zellij unstable.zsh
     unstable.xorg.libX11 unstable.brightnessctl unstable.home-manager
     unstable.gtk-engine-murrine unstable.libsForQt5.qt5ct
-    unstable.tailscale unstable.pcmanfm
+    unstable.tailscale unstable.pcmanfm unstable.qvfs unstable.tumbler
 
     # Stable packages
     stable.docker stable.docker-compose
