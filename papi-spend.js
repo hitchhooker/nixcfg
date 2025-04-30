7bc6fada822b0434a396b5d178e8f767414646c5
hitchhooker <tommi@niemi.lol>
2025-04-30 11:02:58 +0700

    update: /nix/store/j3x4kz1iz4wd7dkdisyviiph3pyl7j30-nixos-system-lenovo-25.05beta768633.c80f6a7e10b3

--- papi-spend.js
+++
@@ -1,6 +0,0 @@
-import { connect } from "polkadot-api";
-const api = await connect("wss://polkadot.dotters.network");
-const spend = await api.query("Treasury", "SpendPeriod");
-console.log(`SpendPeriod Blocks: ${spend}`);
-console.log(`SpendPeriod Days: ${Number(spend) * 6 / 3600 / 24}`);
-process.exit(0);
