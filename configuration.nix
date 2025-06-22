1ef6e350258df79618a53e0584d5235b296f4fd2
hitchhooker <tommi@niemi.lol>
2025-06-22 08:19:14 +0700

    update:

--- configuration.nix
+++ configuration.nix
@@ -213,7 +213,7 @@ in
       export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
       export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
       export SQLITE3_LIB_DIR="${pkgs.sqlite.out}/lib"
-      export LD_LIBRARY_PATH="${pkgs.sqlite.out}/lib:${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH"
+      export LD_LIBRARY_PATH="${pkgs.sqlite.out}/lib:${pkgs.openssl.out}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
       export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
       export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.glibc.dev}/include"
       exec ${pkgs.rustup}/bin/rustup run nightly cargo "$@"
