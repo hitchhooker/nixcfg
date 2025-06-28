3e45cd179a2a4504499c1e80fbaa96423ccc8d64
hitchhooker <tommi@niemi.lol>
2025-06-28 12:15:06 +0700

    update: /nix/store/bh34a2nmkgljmcg9r3419k14plcm0j52-nixos-system-lenovo-25.11pre820854.30a61f056ac4

--- configuration.nix
+++ configuration.nix
@@ -329,11 +329,12 @@ in
     };
 
     # ────────── ollama service ─────────
-    ollama = {
-      enable = true;
-      acceleration = "rocm";
-      listenAddress = "127.0.0.1:11434";
-    };
+    #ollama = {
+    #  enable = true;
+    #  acceleration = "rocm";
+   #   host = "127.0.0.1";
+   #   port = 11434;
+    #};
 
 
     acpid.enable = true;
