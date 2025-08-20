6e2c9ca5cc4a01953ea72fca15bc8aad2908581e
hitchhooker <tommi@niemi.lol>
2025-08-20 13:49:11 +0700

    update:

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -6,5 +6,8 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+~/.local/bin/btc-polybar &
+sleep 1
+pkill btc-polybar
 # Launch Polybar
 polybar top -c ~/.config/polybar/config.ini &
